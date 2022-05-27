a482a56f487550f189e834502020ba9cad21031f
julfinch <julfinch@gmail.com>
2022-05-28 03:59:47 +0800

    v0.23 card icons

--- src/components/advanced/Advanced.jsx
+++ src/components/advanced/Advanced.jsx
@@ -1,5 +1,8 @@
 import AdvancedStyles from "./AdvancedStyles.css";
 import Shortener from "../shortener/Shortener";
+import brand from "../../assets/icon-brand-recognition.svg";
+import details from "../../assets/icon-detailed-records.svg";
+import fully from "../../assets/icon-fully-customizable.svg";
 
 const Advanced = () => {
   return (
@@ -19,6 +22,9 @@ const Advanced = () => {
               </div>
               <div className="card-container">
                 <div className="cards">
+                  <div className="cards-icon">
+                    <img src={brand} alt="brand" />
+                  </div>
                   <h3>Brand Recognition</h3>
                   <p>
                     Boost your brand recognition with each click. Generic links
@@ -27,6 +33,9 @@ const Advanced = () => {
                   </p>
                 </div>
                 <div className="cards">
+                  <div className="cards-icon">
+                    <img src={details} alt="details" />
+                  </div>
                   <h3>Detailed Records</h3>
                   <p>
                     Gain insights into who is clicking your links. Knowing when
@@ -35,6 +44,9 @@ const Advanced = () => {
                   </p>
                 </div>
                 <div className="cards">
+                  <div className="cards-icon">
+                    <img src={fully} alt="fully" />
+                  </div>
                   <h3>Fully Customizable</h3>
                   <p>
                     Improve brand awareness and content discoverability through
