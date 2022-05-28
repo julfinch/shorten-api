8e42d65244540da29dc48c3227ad9a819217eedb
julfinch <julfinch@gmail.com>
2022-05-28 23:52:01 +0800

    v0.33 transfered picture boost inside container

--- src/components/boost/Boost.jsx
+++ src/components/boost/Boost.jsx
@@ -6,11 +6,11 @@ const Boost = () => {
   return (
     <>
       <section className="boost">
-        <picture>
-          <source media="max-width: 768px" srcSet={bgMobile} />
-          <img src={bgDesktop} alt="" />
-        </picture>
         <div className="container">
+          <picture>
+            <source media="max-width: 768px" srcSet={bgMobile} />
+            <img src={bgDesktop} alt="" />
+          </picture>
           <div className="content boost-inner">
             <h2>Boost your links today</h2>
             <button className="btn-2">Get Started</button>
